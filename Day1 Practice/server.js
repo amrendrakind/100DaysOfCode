e2d0f2cc61fcef1741f58c0a1dbc1cf91ab0880c
amrendrakind <vision2010@gmail.com>
2021-09-06 18:59:43 +0530

    Day1 Practice

--- Day1 Practice/server.js
+++ Day1 Practice/server.js
@@ -1,9 +1,22 @@
 import express from 'express'
 const app=express()
+
 app.set('view-engine','ejs')
 
 app.get('/',(req,res)=>{
     res.render('index.ejs',{user:'Amrendra K'})
+})
+
+app.get('/login',(req,res)=>{
+    res.render('login.ejs')
+})
+
+app.get('/register',(req,res)=>{
+    res.render('register.ejs')
+})
+
+app.post('/register',(req,res)=>{
+    res.render('register.ejs')
 })
 
 
