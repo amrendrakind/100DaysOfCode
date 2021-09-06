24b2965a5d4583ff3bbf4231d3674e5bc028936c
amrendrakind <vision2010@gmail.com>
2021-09-06 19:42:14 +0530

    Day1 Practice

--- Day1 Practice/server.js
+++ Day1 Practice/server.js
@@ -1,7 +1,11 @@
 import express from 'express'
+import bcrypt from 'bcrypt'
 const app=express()
 
+const users=[]
 app.set('view-engine','ejs')
+app.use(express.urlencoded({encoded:false}))
+
 
 app.get('/',(req,res)=>{
     res.render('index.ejs',{user:'Amrendra K'})
@@ -15,10 +19,28 @@ app.get('/register',(req,res)=>{
     res.render('register.ejs')
 })
 
-app.post('/register',(req,res)=>{
-    res.render('register.ejs')
+app.post('/register', async(req,res)=>{
+    
+    try {
+        
+        const hashedPassword = await bcrypt.hash(req.body.password,10)
+        users.push({
+            id: Date.now().toString(),
+            name : req.body.name,
+            email : req.body.email,
+            password: hashedPassword
+        })
+        res.redirect('/login')
+
+    } catch (error) {
+        res.redirect('/register')
+    }
+//    console.log(users)
 })
 
+app.post('/login',(req,res)=>{
+
+})
 
 
 app.listen(4000,console.log("Server started"))
