0cc7ed6a776a36262d6cc38dd713239dd1480647
amrendrakind <vision2010@gmail.com>
2021-09-12 02:32:41 +0530

    Day4 of 100

--- Day4 Practice/server.js
+++ Day4 Practice/server.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const cors = require("cors")
 const PORT = process.env.SERVER_PORT || 4040
 
 app.use(express.json());  
 
-//Routes
 
 //app.use("/todo", todosRoutes);            //For todo report by pagination
 
