c4a849eaf24a6ec300f10fd59345b475ea2c469b
amrendrakind <vision2010@gmail.com>
2021-09-12 02:21:52 +0530

    Day4 of 100

--- Day4 Practice/server.js
+++ Day4 Practice/server.js
@@ -1,25 +1,11 @@
-const dotenv = require('dotenv')
 const express = require('express')
 const app = express()
-const Connct_Mongo_DB = require('./config/dbConfig.js')
-const todosRoutes = require('./routes/todo.route.js')
-const bodyParser = require('body-parser')
-const cors = require('cors');
-
-// Initialisation of Mongodb connection
-Connct_Mongo_DB();
-
-if (process.env.NODE_ENV !== 'production') {
-    dotenv.config()
-}
 const PORT = process.env.SERVER_PORT || 4040
 
-app.use(bodyParser.json());
 app.use(express.json());  
-app.use(cors());
 
 //Routes
 
-app.use("/todo", todosRoutes);            //For todo report by pagination
+//app.use("/todo", todosRoutes);            //For todo report by pagination
 
-app.listen(PORT, console.log(`server is running on port ${PORT}`))
+app.listen(PORT, console.log(`server is running on port ${PORT}`)) 
