8a002fbf8aaccd2463e2900b15a0085cb1adfa47
amrendrakind <vision2010@gmail.com>
2021-09-12 02:44:31 +0530

    Day4 of 100

--- Day4 Practice/server.js
+++ Day4 Practice/server.js
@@ -2,9 +2,17 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require("cors")
-const PORT = process.env.SERVER_PORT || 4040
+const PORT = process.env.SERVER_PORT || 8080
 
+var corsOptions={ origin : "http://localhost:8081" }
+
+app.use(cors(corsOptions))
 app.use(express.json());  
+app.use(express.urlencoded({extended: true}))
+
+//Welcome Message
+
+app.get("/",(req,res)=>{res.json({message : "Welcome to Amrendra's World!!"})})
 
 
 //app.use("/todo", todosRoutes);            //For todo report by pagination
