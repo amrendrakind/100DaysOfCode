a39e3cb58659aa7d0856f6d3b3e410a58b941919
amrendrakind <vision2010@gmail.com>
2021-09-12 03:09:04 +0530

    Day4 of 100

---
+++ Day4 Practice/config/models/index.js
@@ -0,0 +1,23 @@
+const dbConfig = require("../config/dbConfig.js");
+
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+  host: dbConfig.HOST,
+  dialect: dbConfig.dialect,
+
+  pool: {
+    max: dbConfig.pool.max,
+    min: dbConfig.pool.min,
+    acquire: dbConfig.pool.acquire,
+    idle: dbConfig.pool.idle
+  }
+});
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.tutorials = require("./todos.model.js")(sequelize, Sequelize);
+
+module.exports = db;
